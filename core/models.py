6a1a9c64abcbea1aef0c9f5d70e7752c318326ef
vivek <vivekwarkade000@gmail.com>
2022-06-20 23:12:58 +0530

    add models opts

--- core/models.py
+++ core/models.py
@@ -1,13 +1,29 @@
+from sre_parse import CATEGORIES
 from sys import flags
 from turtle import title
+from unicodedata import category
 from django.db import models
 from django.conf import settings
 
 # Create your models here.
 
+CATEGORIES_CHOICES = (
+    ('S', 'Shirt'),
+    ('SM', 'Sport wear'),
+    ('OW', 'Outwear')
+)
+
+LABEL_CHOICES = (
+    ('P', 'Primary'),
+    ('S', 'Secondary'),
+    ('D', 'danger')
+)
+
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    category = models.CharField(choices=CATEGORIES_CHOICES, max_length=2 , default=())
+    lable = models.CharField(choices=LABEL_CHOICES, max_length=1, default=())
 
     def _str_(self):
       return self.title
